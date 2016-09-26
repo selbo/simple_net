319f334b87d0a1b42603de164e94ebfdaa104729
Ahuva Kroizer <ahuva.kroizer@intel.com>
2016-09-26 15:13:32 +0300

    simple echo server

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import json
 import websocket_server
 from StringIO import StringIO
 
+clients=[]
 
 def register(id):
     pass
@@ -24,17 +25,27 @@ _on_msg_callbacks=[]
 def on_msg(call_back):
     _on_msg_callbacks.append(call_back)
 
+def on_message(client, server, msg):
+    global gserver
+    print("received message {0}".format(msg))
+    gserver.send_message_to_all(msg)
+
 from websocket_server import WebsocketServer
 
 def new_client(client, server):
     #server.send_message_to_all("Hey all, a new client has joined us")
-    send("led", "off", client)
+    print("new connection received")
+    clients.append(client)
+
+def leaving_client(client,server):
+    clients.remove(client)    
 
 if __name__ == "__main__":
     global gserver
-    print("test")
     gserver = WebsocketServer(8000)
     gserver.set_fn_new_client(new_client)
+    gserver.set_fn_message_received(on_message)
+    gserver.set_fn_client_left(leaving_client)
     gserver.run_forever()
 
 
