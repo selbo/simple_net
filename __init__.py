988a5983dc1e6718702930001e72848ed68aacd6
serans1 <eran.shlomo@intel.com>
2016-09-26 12:00:43 +0300

    adding first websocket communication

--- __init__.py
+++ __init__.py
@@ -11,5 +11,15 @@ _on_msg_callbacks=[]
 def on_msg(call_back):
     _on_msg_callbacks.append(call_back)
 
+from websocket_server import WebsocketServer
+
+def new_client(client, server):
+    server.send_message_to_all("Hey all, a new client has joined us")
+
+if __name__ == "__main__":
+    server = WebsocketServer(13254)
+    server.set_fn_new_client(new_client)
+    server.run_forever()
+
 
 
